4c16784c0a9a06cd7e99f5521df2a5d779beb6c0
Clint Sbisa <csbisa@gmail.com>
2018-03-24 11:18:02 -0700

    process: Add superblock support
    Data may span over multiple subblocks, so handling has been added to
    the processor which will now reassemble those blocks before invoking
    the callback.
    
    The definition for (the badly named) mh4u_data has been revised so it
    no longer parses type as the presence of that field is
    context-dependent, so dependent code now parses it manually.

--- mh_types/mh4u_data.ksy
+++ mh_types/mh4u_data.ksy
@@ -20,7 +20,7 @@ types:
       - id: length
         type: u2
       - id: seqnum
-        size: 4
+        type: u4be
       - id: random
         size: 8
       - id: data
@@ -31,27 +31,33 @@ types:
         value: (((flags & 0x40) >> 6) * 0x100) + length
   subblockhdr:
     seq:
-    - id: unknown1
-      type: u2
-    - id: unknown2
-      type: u2
+    - id: seq1
+      type: u2be
+    - id: seq2
+      type: u2be
     - id: unknown3
       type: u1
     - id: count
       type: u1
     - id: blocks
       type: subblock
-      repeat: eos
+      repeat: expr
+      repeat-expr: count
   subblock:
     seq:
-      - id: unknown1
+      - id: flags
         type: u1
-      - id: length
+        doc: |
+          * 0x02: Health check packet
+          * 0x10: Empty subblock (accompanied by length of 0xFE or 0xFF).
+          * 0x20: Initial transmit. On retransmits, this bit is not set.
+          * 0x40: Data spans multiple subblocks.
+      - id: length_spec
         type: u1
-      - id: unknown2
-        type: u2
-      - id: type
-        type: u2
+        doc: Specifies the length of the data. If this value is 0xFE or 0xFF,
+             the length of the data is 4.
       - id: data
-        size: length-4
-        if: length != 0xFF and length != 0xFE and length >= 4
+        size: length
+    instances:
+      length:
+        value: '(length_spec & 0xFE) != 0xFE ? length_spec : 4'
--- poogie/process.py
+++ poogie/process.py
@@ -1,7 +1,7 @@
 from mh_types.generated.mh4u_remote_packet import Mh4uRemotePacket
 from mh_types.generated.mh4u_data import Mh4uData
 from scapy.utils import PcapReader
-from scapy.layers.inet import UDP, Raw
+from scapy.layers.inet import IP, UDP, Raw
 
 def generate_xor_bytes(byte1, byte2):
     seed = byte1 + (byte2 << 8)
@@ -33,26 +33,40 @@ def try_parse(data):
             pass
     raise
 
-class MHPacketProcessor():
-    base_time = 0
-    unrelated_packets = 0
-    failed_remote_packet_load = 0
-    failed_block_parse = 0
-    parsed_subblocks = 0
+def generate_stream_key(pkt):
+    return str(pkt[IP].src) + str(pkt[UDP].sport) + '->' + str(pkt[IP].dst) + str(pkt[UDP].dport)
 
+class MHPacketProcessor():
     def __init__(self, callback):
         self.callback = callback
 
-    def process(self, pkt, verbose=False, **kwargs):
+        self.base_time = 0
+        self.current_superblocks = {}
+
+        # initialize counters
+        self.failed_remote_packet_load = 0
+        self.failed_block_parse = 0
+        self.parsed_subblocks = 0
+        self.superblock_count = 0
+        self.pkt_count = 0
+        self.duplicate_subblocks = 0
+        self.unrelated_packets = 0
+
+    def process(self, pkt, verbose=False, stateful=True, **kwargs):
         """Processes a single scapy packet.
 
         If the packet is determined to be related, the object's callback
-        function will be called with a Subblock and relative time for
-        the packet, as well as any additional keyword arguments provided
-        to this function.
+        function will be called with a data chunk, flags and relative
+        time for the packet, as well as any additional keyword arguments
+        provided to this function.
+
+        In stateful mode, superblocks will be parsed and the callback
+        will only be invoked with assembled superblocks and not with
+        partial superblocks.
         """
         if self.base_time == 0:
             self.base_time = pkt.time
+        self.pkt_count += 1
 
         if UDP not in pkt or Raw not in pkt or pkt[Raw].load[0:2] != b'\xea\xd0':
             return
@@ -61,14 +75,16 @@ class MHPacketProcessor():
             parsed = Mh4uRemotePacket.from_bytes(pkt[Raw].load)
         except:
             self.failed_remote_packet_load += 1
+            #self.failed_pkts.append(pkt)
             if verbose:
-                print("Failed to parse remote packet")
+                print("Failed to parse remote packet from", pkt[IP].src)
+                print(pkt[Raw].load)
             return
 
         if parsed.flags != 0x882:
             self.unrelated_packets += 1
             if verbose:
-                print("Skipped unrelated packet")
+                print("Skipped unrelated packet from", pkt[IP].src)
             return
 
         for block in parsed.data_blocks.data_blocks:
@@ -77,17 +93,61 @@ class MHPacketProcessor():
                 parsed_block = try_parse(block)
             except:
                 self.failed_block_parse += 1
+                #self.failed_pkts.append(pkt)
                 if verbose:
                     print("Failed to parse block")
                 return
 
             for r in parsed_block.blocks:
                 for s in r.data.blocks:
+                    if not (s.flags & 0x20):
+                        # Skipping dupes
+                        self.duplicate_subblocks += 1
+                        continue
+
                     self.parsed_subblocks += 1
-                    self.callback(s, time=pkt.time - self.base_time, **kwargs)
+                    handled = stateful and self._handle_superblock(s, pkt, **kwargs)
+                    if not handled:
+                        self.callback(s.data, s.flags, time=pkt.time - self.base_time, **kwargs)
+
+    # Data may span over multiple subblocks, which is indicated by 0x40 in the
+    # subblock flags. For lack of a better name, we call those superblocks.
+    # Determining the end of the su[erblock appears to be context-dependent, so
+    # we just use the simple heuristic of terminating the superblock once a
+    # subblock of non-maximal length is processed.
+    def _handle_superblock(self, s, pkt, **kwargs):
+        if s.length == 0xFF or s.length == 0xFE:
+            return False
+        if not (s.flags & 0x40):
+            return False
+
+        key = generate_stream_key(pkt)
+        sb = self.current_superblocks.get(key)
+        if sb is None:
+            #print("Found start of a running subblock")
+            sb = [s]
+            self.current_superblocks[key] = sb
+        else:
+            #print("Appending running subblock")
+            sb.append(s)
+
+        if s.length == 192:
+            #print("Running subblock isn't complete yet, added:")
+            #self.callback(s.data, self.current_superblock[0].flags, time=pkt.time - self.base_time, **kwargs)
+            return True
+        #print("Found running subblock end, its length is", s.length)
+        self.superblock_count += 1
+        #print("Calling back with running subblock of length", len(self.current_superblock))
+        data = b''.join([d.data for d in sb])
+        self.callback(data, sb[0].flags, time=pkt.time - self.base_time, **kwargs)
+        del self.current_superblocks[key]
+        return True
 
     def get_stats(self):
         return {'parsed_subblocks': self.parsed_subblocks,
                 'unrelated_packets': self.unrelated_packets,
                 'failed_remote_packet_load': self.failed_remote_packet_load,
-                'failed_block_parse': self.failed_block_parse}
+                'failed_block_parse': self.failed_block_parse,
+                'superblocks': self.superblock_count,
+                'packets': self.pkt_count,
+                'duplicate_subblocks': self.duplicate_subblocks}
